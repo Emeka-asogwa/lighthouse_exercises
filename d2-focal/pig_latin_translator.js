d56d820c2bb20d36003719ccb68057416a6c8e6c
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-05-26 19:56:52 +0000

    weekly exercises

---
+++ d2-focal/pig_latin_translator.js
@@ -0,0 +1,16 @@
+var originalWords = process.argv.slice(2);
+var pigLatinWords = [];
+
+function translateToPigLatin(word) {
+  return word.slice(1, word.length) + word[0] + "ay";
+}
+
+
+for (var i = 0; i < originalWords.length; i++) {
+  //console.log(translateToPigLatin(originalWords[i]));
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(' '));
+
+
